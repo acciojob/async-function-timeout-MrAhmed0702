b829ec1323601e13f0f06c6f5d98998c631a5310
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-09-29 05:13:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+const button = document.getElementById("btn");
+const text = document.getElementById("text");
+const delay = document.getElementById("delay");
+const content = document.getElementById("output");
+
+function wait(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function display(message, delayTime) {
+  await wait(delayTime);
+  content.textContent = message;
+}
+
+button.addEventListener('click', () => {
+  const message = text.value;
+  const delayTime = Number(delay.value);
+  content.textContent = "";
+  display(message, delayTime);
+});
